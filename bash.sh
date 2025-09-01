49bc1b3c023e1fe59596c80e10351363aabe4a18
Lukman Kunveng <123512811+MrKunveng@users.noreply.github.com>
2025-09-01 11:31:16 +0000

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,6 @@
+youtube-downloader/
+├─ app.py                   # your code (rename main file to app.py)
+├─ requirements.txt
+├─ packages.txt
+├─ runtime.txt              # optional, pins Python
+└─ README.md                # optional
